95a573ed3298e4738e893b35281f5b758382a976
Nataa19 <canteronatanael8@gmail.com>
2025-04-27 16:28:37 -0300

    se añadió init.sh

--- setup.sh
+++ setup.sh
@@ -85,29 +85,5 @@ apt install net-tools -y
 
 #Añadimos el user 'nginx' y lo agregamos al grupo docker
 
-adduser --quiet --disabled-password --gecos "" nginx && echo nginx:nginx | chpasswd && sudo usermod -aG docker nginx && newgrp docker
-
-#EXTRA
-#Descargamos el repo en .zip desde mi GitHub para poder ejecutar mi docker-compose
-
-mkdir -p /home/nginx
-
-wget -P /home/nginx https://github.com/Nataa19/my-app/archive/refs/heads/main.zip
-
-cd /home/nginx && unzip main.zip
-
-#Descargamos el cliente DUC para NO-IP para poder ejecutar el contenedor con la DNS
-
-wget -P /home/nginx --content-disposition https://www.noip.com/download/linux/latest
-
-cd /home/nginx && tar xf noip-duc_3.3.0.tar.gz
-
-cd noip-duc_3.3.0/binaries && sudo apt install ./noip-duc_3.3.0_amd64.deb
-
-chmod 777 /home/nginx/main.zip
-chmod -R 777 /home/nginx/my-app-main
-chmod 777 /home/nginx/noip-duc_3.3.0.tar.gz
-chmod -R 777 /home/nginx/noip-duc_3.3.0
-
-echo noip-duc -g all.ddnskey.com --username pzvfdr6 --password wcd8dNYoLeqD > /home/nginx/initduc.txt 
+adduser --quiet --disabled-password --gecos "" --home /home/nginx nginx && echo nginx:nginx | chpasswd && sudo usermod -aG docker nginx && newgrp docker
 
